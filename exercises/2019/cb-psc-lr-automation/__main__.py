6f7a63c2b4a9c0ee987735fb05f20627e410cafa
Jason Garman <jgarman@carbonblack.com>
2019-06-02 16:23:19 -0400

    add documentation, clean up

--- exercises/2019/cb-psc-lr-automation/__main__.py
+++ exercises/2019/cb-psc-lr-automation/__main__.py
@@ -1,10 +1,39 @@
-from notifications import NotificationListener, SyntheticNotificationListener
+#!/usr/bin/env python
+
+"""
+This script demonstrates how to automate a Live Response action when a notification
+is received on a given device.
+
+To follow along, start at the ``main()`` function at the bottom of this file, then
+trace through the ``LiveResponseOrchestrator.start()`` method.
+
+This script demonstrates how you can implement a simple orchestration capability
+with the Carbon Black Predictive Security Cloud. This orchestration example is
+subdivided into four parts. Each of the parts demonstrated
+are modular, so you can swap out for different functionality. The high level
+modules demonstrated here are:
+
+``actions`` - Implements the Live Response worker; change this out if you want to
+adjust the data that you want to collect on the remote endpoint.
+
+``triggers`` - Provides two implementations of a 'trigger' mechanism: one
+will poll the PSC Notifications API to trigger on any alert/incident notification,
+and the other will generate testing notifications to trigger on a specific device
+ID.
+
+``datastore`` - Implements a simple in-memory database to store the results of
+the retrieved Live Response data. Change this out if you want to persist the data
+into an external database.
+
+``webapi`` - Provides the web front-end to view the results stored in the database.
+"""
+
 from actions import CollectContextWorker
+from triggers import NotificationListener, SyntheticNotificationGenerator
 from datastore import DataStore
 from webapi import get_flask_server
 
 from cbapi.example_helpers import get_cb_defense_object, build_cli_parser
-from six.moves.queue import Queue
 
 import sys
 import logging
@@ -29,19 +58,22 @@ class LiveResponseOrchestrator(object):
 
         # Start the thread to listen to notifications
         if self.args.synthetic:
-            notification_thread = SyntheticNotificationListener(self.args.synthetic, self.process_notification,
-                                                                poll_interval=poll_interval)
+            notification_thread = SyntheticNotificationGenerator(self.args.synthetic, self.datastore,
+                                                                 poll_interval=poll_interval)
         else:
-            notification_thread = NotificationListener(self.cb, self.process_notification, poll_interval=poll_interval)
+            notification_thread = NotificationListener(self.cb, self.datastore, poll_interval=poll_interval)
+        notification_thread.start()
 
         # Start five threads to listen for work and do the Live Response
         for i in range(5):
             worker_thread = CollectContextWorker(self.cb, self.datastore)
             worker_thread.start()
 
-        notification_thread.start()
+        # Sleep forever so our worker threads do all the work in the background. Listen for a keyboard
+        #  interrupt to exit cleanly
         try:
-            time.sleep(100)
+            while True:
+                time.sleep(1)
         except KeyboardInterrupt:
             self.datastore.dump_info()
             return
@@ -55,20 +87,25 @@ class LiveResponseOrchestrator(object):
 
 
 def main():
+    # First we create a command line parser to collect our configuration.
+    # We use the built in ``build_cli_parser`` in ``cbapi.example_helpers`` to build
+    #  the basic command line parser, then augment with a few parameters specific to
+    #  this script.
     parser = build_cli_parser("Example CB PSC Live Response automation")
     parser.add_argument("--synthetic", help="Generate synthetic notifications with given Device ID",
                         metavar="DEVICE_ID")
     parser.add_argument("--poll", help="Poll interval for the Notifications API", type=int, default=30)
+    parser.add_argument("--siemprofile", help="CB Profile for SIEM API key (required to poll for notifications)",
+                        default="siem")
     args = parser.parse_args()
-
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.INFO)
 
     log.info("Starting")
 
     orchestrator = LiveResponseOrchestrator(args)
-
     orchestrator.start()
 
 if __name__ == '__main__':
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+
     sys.exit(main())
--- exercises/2019/cb-psc-lr-automation/datastore.py
+++ exercises/2019/cb-psc-lr-automation/datastore.py
@@ -8,6 +8,33 @@ log = logging.getLogger(__name__)
 
 
 class DataStore(object):
+    """A simple object to track two things: the queue of devices we will be interrogating
+    to collect Live Response results, and a database of the results we receive from those
+    devices.
+
+    The interface to this object is simple: use the ``enqueue_work()`` function to add
+    a device ID into the list of devices to operate upon. If the device is already in the
+    queue, this function returns immediately so as to avoid making multiple simultaneous
+    requests to the same device.
+
+    Worker threads can "pick up" work by calling ``dequeue_work()`` which sleeps until a
+    work item is ready. Once a work item is ready, the device ID is returned.
+
+    Once results are received from the remote device, the worker thread calls the
+    ``update_result()`` function to add information to the result database for that device
+    ID.
+
+    The ``work_done()`` function should then be called to signal that the Live Response
+    session is complete (whether it was successful or not). The device ID is removed from
+    the list of devices being operated upon, so future calls to ``enqueue_work()`` with
+    the same device ID will cause a new Live Response session for that device ID.
+
+    Results can be consumed through the ``devices_available()`` method, which will return
+    a list of all device IDs with results available in the database, and the
+    ``get_result()`` method which will return the dictionary of detailed information for
+    a given device ID.
+    """
+
     def __init__(self):
         self.live_response_results = defaultdict(dict)
         self.live_response_lock = threading.Lock()
@@ -28,7 +55,7 @@ class DataStore(object):
 
     def devices_available(self):
         with self.live_response_lock:
-            res = self.live_response_results.keys()
+            res = list(self.live_response_results.keys())
 
         return res
 
